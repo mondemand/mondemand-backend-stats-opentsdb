479cdfd4fe557eeb991671f33a9eb86c7b1a39be
Anthony Molinaro <anthony.molinaro@openx.com>
2014-05-22 14:50:01 -0700

    update to work with new behavior

--- src/mondemand_backend_stats_opentsdb.erl
+++ src/mondemand_backend_stats_opentsdb.erl
@@ -1,14 +1,14 @@
 -module (mondemand_backend_stats_opentsdb).
 
--behaviour (gen_server).
+-behaviour (supervisor).
 -behaviour (mondemand_server_backend).
 -behaviour (mondemand_backend_stats_handler).
 
 %% mondemand_server_backend callbacks
 -export ([ start_link/1,
            process/1,
-           stats/0,
-           required_apps/0
+           required_apps/0,
+           type/0
          ]).
 
 %% mondemand_backend_stats_handler callbacks
@@ -19,66 +19,50 @@
            handle_response/2
          ]).
 
-%% gen_server callbacks
--export ([ init/1,
-           handle_call/3,
-           handle_cast/2,
-           handle_info/2,
-           terminate/2,
-           code_change/3
-         ]).
-
--record (state, { sidejob,
-                  stats = dict:new ()
-                }).
+%% supervisor callbacks
+-export ([init/1]).
 
 %%====================================================================
 %% mondemand_server_backend callbacks
 %%====================================================================
 start_link (Config) ->
-  gen_server:start_link ( { local, ?MODULE }, ?MODULE, Config, []).
+  supervisor:start_link ( { local, ?MODULE }, ?MODULE, [Config]).
 
 process (Event) ->
-  mondemand_backend_connection_pool:cast (?MODULE, {process, Event}).
-
-stats () ->
-  gen_server:call (?MODULE, {stats}).
+  mondemand_backend_worker_pool_sup:process
+    (mondemand_backend_stats_opentsdb_worker_pool, Event).
 
 required_apps () ->
-  [ sidejob ].
+  [ ].
+
+type () ->
+  supervisor.
 
 %%====================================================================
-%% gen_server callbacks
+%% supervisor callbacks
 %%====================================================================
-init (Config) ->
-  Limit = proplists:get_value (limit, Config, 10),
-  Number = proplists:get_value (number, Config, undefined),
+init ([Config]) ->
+  Number = proplists:get_value (number, Config, 16), % FIXME: replace default
 
-  { ok, Proc } =
-    mondemand_backend_connection_pool:init ([?MODULE, Limit, Number]),
-  { ok, #state { sidejob = Proc } }.
-
-handle_call ({stats}, _From, State) ->
-  Stats = mondemand_backend_connection_pool:stats (?MODULE),
-  { reply, Stats, State };
-handle_call (Request, From, State) ->
-  error_logger:warning_msg ("~p : Unrecognized call ~p from ~p~n",
-                            [?MODULE, Request, From]),
-  { reply, ok, State }.
-
-handle_cast (Request, State) ->
-  error_logger:warning_msg ("~p : Unrecognized cast ~p~n",[?MODULE, Request]),
-  { noreply, State }.
-
-handle_info (Request, State) ->
-  error_logger:warning_msg ("~p : Unrecognized info ~p~n",[?MODULE, Request]),
-  {noreply, State}.
-
-terminate (_Reason, #state { }) ->
-  ok.
-
-code_change (_OldVsn, State, _Extra) ->
-  {ok, State}.
+  { ok,
+    {
+      {one_for_one, 10, 10},
+      [
+        { mondemand_backend_stats_opentsdb_worker_pool,
+          { mondemand_backend_worker_pool_sup, start_link,
+            [ mondemand_backend_stats_opentsdb_worker_pool,
+              mondemand_backend_connection,
+              Number,
+              ?MODULE ]
+          },
+          permanent,
+          2000,
+          supervisor,
+          [ ]
+        }
+      ]
+    }
+  }.
 
 %%====================================================================
 %% mondemand_backend_stats_handler callbacks
